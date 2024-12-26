d02ae7bb5907305042a907514b8edd4251708b32
Fatemeh <fatemeh@Mac-Book-Pro-M1-2020.local>
2024-12-25 22:33:51 -0800

    Handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    let input = input.trim();
+
+    if !input.starts_with("invalid") {
+        println!("{}", input);
+    } else {
+        println!("{}: command not found", input);
+    }
 }
