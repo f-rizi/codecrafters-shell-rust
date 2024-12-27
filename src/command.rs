ce5bc70c5fe7143c3a651151da5b006786232eeb
Fatemeh <fatemeh@Mac-Book-Pro-M1-2020.local>
2024-12-26 22:50:09 -0800

    codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -1,44 +1,112 @@
-#[derive(Clone, Debug)]
-pub enum Command {
-    Echo { message: Vec<String> },
+use std::process;
+
+#[derive(Clone, Debug, Default)]
+pub enum Type {
+    Echo,
+    Exit,
+    Type,
     Invalid,
-    Exit { message: String },
-    Unknown(Vec<String>),
+    #[default]
+    Unknown,
+}
+
+#[derive(Clone, Debug, Default)]
+pub enum BuildType {
+    Builtin,
+    NoFound,
+    #[default]
+    Unknown,
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct Command {
+    pub command_type: Type,
+    pub build_type: BuildType,
+    pub parameters: Vec<String>,
 }
 
 impl Command {
-    pub fn parse(parts: &Vec<String>) -> Self {
+    pub fn new(parts: &Vec<String>) -> Self {
+        if parts.len() == 0 {
+            return Command::default();
+        }
+
         let cmd = parts[0].to_lowercase();
 
         if cmd.starts_with("invalid") {
-            if parts.len() == 1 {
-                return Command::Invalid;
-            } else {
-                return Command::Unknown(parts.to_vec());
-            }
+            return Self {
+                command_type: Type::Invalid,
+                build_type: BuildType::NoFound,
+                parameters: vec![cmd],
+            };
         }
+
+        let mut result = Command::default();
 
         match cmd.as_str() {
+            "type" => {
+                if parts.len() < 2 {
+                    return result;
+                }
+
+                result.command_type = Type::Type;
+                result.build_type = BuildType::Builtin;
+                result.parameters = parts[1..].to_vec();
+            }
             "echo" => {
                 if parts.len() < 2 {
-                    return Command::Unknown(parts.clone());
+                    return result;
                 }
 
-                Command::Echo {
-                    message: parts[1..].to_vec(),
-                }
+                result.command_type = Type::Echo;
+                result.build_type = BuildType::Builtin;
+                result.parameters = parts[1..].to_vec();
             }
             "exit" => {
-                if parts.len() != 2 {
-                    return Command::Unknown(parts.to_vec());
+                if parts.len() < 2 {
+                    return result;
                 }
 
-                let message = parts[1].clone();
-                Command::Exit { message }
+                result.command_type = Type::Exit;
+                result.build_type = BuildType::Builtin;
+                result.parameters = parts[1..].to_vec();
             }
             _ => {
-                return Command::Unknown(parts.to_vec());
+                return result;
             }
         }
+        result
+    }
+
+    pub fn execute(&self) {
+        match self.command_type {
+            Type::Echo => {
+                for (index, item) in self.parameters.iter().enumerate() {
+                    print!("{}", item);
+
+                    if index != self.parameters.len() - 1 {
+                        print!(" ");
+                    }
+                }
+            }
+            Type::Exit => {
+                // process::exit(1);
+                process::abort();
+            }
+            Type::Invalid => {
+                print!("{}: command not found", self.parameters[0]);
+            }
+            Type::Type => match self.build_type {
+                BuildType::Builtin => {
+                    print!("{} is a shell builtin", self.parameters[0]);
+                }
+                BuildType::NoFound => {
+                    print!("{}: not found", self.parameters[0])
+                }
+                _ => {}
+            },
+            Type::Unknown => {}
+        }
+        println!();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod command;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-use command::Command;
+use command::{Command, Type};
 
 fn main() {
     loop {
@@ -17,27 +17,7 @@ fn main() {
         let input = input.trim();
 
         let parts: Vec<String> = input.split_whitespace().map(String::from).collect();
-        let command: Command = Command::parse(&parts);
-
-        match command {
-            Command::Echo { message } => {
-                for (index, item) in message.iter().enumerate() {
-                    print!("{}", item);
-
-                    if index != message.len() - 1 {
-                        print!(" ");
-                    }
-                }
-            }
-            Command::Exit { message } => {
-                break;
-            }
-            Command::Invalid => {
-                print!("{}: command not found", input);
-            }
-            _ => {}
-        }
-
-        println!();
+        let command: Command = Command::new(&parts);
+        command.execute();
     }
 }
