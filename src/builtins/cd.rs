c866d87dfe70dd857688dc1d1140f104c24f4f2c
Fatemeh <fatemeh@Mac-Book-Pro-M1-2020.local>
2024-12-27 18:52:51 -0800

    Step: The cd builtin: Home directory

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -13,15 +13,29 @@ impl Command for CD {
 
         let path = &args[0];
 
+        if path == "~" {
+            let home_path = env::var("HOME").ok();
+
+            if let Some(home) = home_path {
+                self.go_to_directory(&home);
+            }
+        } else {
+            self.go_to_directory(&path);
+        }
+    }
+
+    fn name(&self) -> &str {
+        "pwd"
+    }
+}
+
+impl CD {
+    fn go_to_directory(&self, path: &String) {
         match env::set_current_dir(&path) {
             Ok(_) => {}
             Err(_) => {
                 println!("cd: {}: No such file or directory", path);
             }
         }
-    }
-
-    fn name(&self) -> &str {
-        "pwd"
     }
 }
